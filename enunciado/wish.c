b5fd42034a606083fe3ef8e0c2a310243873d918
mBaenaCh <mateo.baena@hotmail.com>
2021-05-07 17:53:24 -0500

    Getting the line and parsing it

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -1,28 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-#define MAX_SIZE 100
-
+/*Variables globales*/
+#define MAX_SIZE 100 //Consideracion de buffer para la linea de entrada de la consola
+#define TOK_DELIM " \t\r\n\a" //Delimitadores para STRTOK
+#define ERROR_MSG "An error has occurred\n" //Mensaje de error global
 
 char *system_commands[] = {
-    "/bin/"
+    "/bin/",
+    NULL
 };
 
-typedef enum{endup,cd, ls, wc, not_command} builtin_command;
+typedef enum{cd, path, endup, not_command} builtin_command;
 
 const static struct{
     builtin_command command;
     char* string_command;
 } commands[] = {
     {cd, "cd"},
-    {ls, "ls"},
-    {wc, "wc"},
+    {path, "path"},
     {endup, "exit"}
 };
 
 builtin_command str_to_command(char * str_command){
-    for(int i=0; i<4; i++){
+    for(int i=0; i<3; i++){
+        int comp = strcmp(str_command,commands[i].string_command);
         if(!strcmp(str_command,commands[i].string_command)){
             return commands[i].command;
         }
@@ -31,28 +35,51 @@ builtin_command str_to_command(char * str_command){
 }
 
 int main(int argc, char*argv[]){
-    char str[MAX_SIZE];
-
+    
+    /*Validar en funcion de argumentos ingresados
+    - argc = 0 -> Modo interactivo: Comando externo o built in
+    - argc = 1 -> Modo batch: Se recibe un archivo como argumento
+    - argc > 1 -> Error
+    */    
+    
+    /*Modo interactivo*/
+    char line[MAX_SIZE];
+    
     do{
         printf("wish> ");
-        fgets(str, MAX_SIZE, stdin);
-        char * p = str;
+
+        /*Obtencion de la linea de texto recibida en consola*/
+        fgets(line, MAX_SIZE, stdin);
+        char * p = line;
         while(*p != '\n'){
             p++;
         }
         *p = '\0';
-        builtin_command command = str_to_command(str);
-        
+
+        /*Separacion de la linea entre comandos y argumentos*/
+        char * command_array[sizeof(line)];
+        char * token;
+        int position = 0;
+        token = strtok(line, TOK_DELIM);
+        while (token != NULL){
+            command_array[position] = token;
+            position++;
+            token = strtok(NULL, TOK_DELIM);
+        }
+        command_array[position] = NULL;
+
+        /*Comparacion linea de comando con comandos built-in*/
+        builtin_command command = str_to_command(command_array[0]);
+
+        /*Filtrado segun comando ingresado*/
         if(command != not_command){
+            //Comando built-in
             switch(command){
-                case cd:   
-                    printf("cd\n");
+                case cd:  
+                    printf("Recibo el comando %s y el argumento %s \n", command_array[0], command_array[1]);
                     break;
-                case ls:
-                    printf("ls\n");
-                    break;
-                case wc:
-                    printf("wc\n");
+                case path:
+                    printf("path\n");
                     break;
                 case endup:
                     exit(0);
@@ -61,8 +88,9 @@ int main(int argc, char*argv[]){
                     printf("command not found\n");
             }
         } else {
-            /*Ejecucion de comandos que no son built in*/
+            /*Comando externo*/
+            printf("El comando ingresado no es built-in\n");
+
         }
-        
     }while(1);
 }
